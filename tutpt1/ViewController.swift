0323562de2d71c6d1b83e01d3058d81b2d030b76
Eric Pan <ericpan@gmail.com>
2014-06-05 10:45:48 -0700

    shows table of 10 cells. end of tutorial part 1

--- tutpt1/ViewController.swift
+++ tutpt1/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // This method asks for the number of rows in our section, normally the length of an array controller
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 10 // Number of rows hard-coded to 10
+    }
+    
+    // Create a new instance of UITableViewCell called cell, using Subtitle cell style
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
 }
 
