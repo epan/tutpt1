9090c165252bb7335f1b97f68ec33c54f0a2e19d
epan <ericpan@gmail.com>
2014-06-07 01:13:08 -0700

    Stuck with trackName String error EXC_BAD_INSTRUCTION

--- tutpt1/ViewController.swift
+++ tutpt1/ViewController.swift
@@ -10,12 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet var appsTableView : UITableView = nil
+    // @IBOutlet var appsTableView : UITableView
     var data: NSMutableData = NSMutableData()
     var tableData: NSArray = NSArray()
-    
-    @IBOutlet var appsTableView : UITableView {
-    
-    }
     
     func searchItunesFor(searchTerm: String) {
         // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
@@ -61,7 +59,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        searchItunesFor("JQ Software")
+        searchItunesFor("Angry Birds")
         
         // Keep track of and contain the response
         var data: NSMutableData = NSMutableData()
@@ -75,15 +73,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     // This method asks for the number of rows in our section, normally the length of an array controller
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return 10 // Number of rows hard-coded to 10
+        return tableData.count
     }
     
     // Create a new instance of UITableViewCell called cell, using Subtitle cell style
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
         
-        cell.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel.text = "Subtitle #\(indexPath.row)"
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.text = rowData["trackName"] as String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        var urlString: NSString = rowData["artworkURL60"] as NSString
+        var imgURL:NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        var imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel.text = formattedPrice
         
         return cell
     }
